dc487f02c6a305623e93eaabe0b34326e20f8351
Sandipan Nag - Supplier <vn08sef@uk.wal-mart.com>
2022-11-23 14:05:35 +0000

    bug fix

--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -1,17 +1,26 @@
 import { Action } from "../actions/index";
-import {ActionType} from '../action-type/index'
+import { ActionType } from '../action-type/index'
 
 const initialState = 0;
 
-export const bankReducer =(state: number = initialState , action:Action) => {
-    switch (action.type){
+export const bankReducer = (state: number = initialState, action: Action) => {
+    switch (action.type) {
         case ActionType.DEPOSIT:
             return state + action.payload;
-        case ActionType.WITHDRAW :
-            return state - action.payload;
+        case ActionType.WITHDRAW:
+            {
+                if (action.payload > state) {
+                    alert("No money to withdraw");
+                    return state;
+
+                } else {
+                    return state - action.payload;
+                }
+            }
         case ActionType.BANKRUPT:
+            alert("All your money is gone");
             return 0;
         default:
-            return state;            
+            return state;
     }
 }
